4615a85c37bfef7a51a4b402c7fc9afd0f10e26c
marom <maro.muszynski@gmail.com>
2019-12-28 20:14:43 +0000

    get limits values based on application.properties configuration

--- limits-service/src/main/java/com/marom/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/marom/limitsservice/LimitsConfigurationController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitsConfigurationController {
 
+    private Configuration configuration;
+
+    public LimitsConfigurationController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
-        return new LimitConfiguration(1000, 1);
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 
 }

d2a652e086b3adc737bb9836859f84965d4acec7
marom <maro.muszynski@gmail.com>
2019-12-28 19:51:12 +0000

    create a simple controller to get hardcoded limits values

---
+++ limits-service/src/main/java/com/marom/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,15 @@
+package com.marom.limitsservice;
+
+import com.marom.limitsservice.bean.LimitConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(1000, 1);
+    }
+
+}

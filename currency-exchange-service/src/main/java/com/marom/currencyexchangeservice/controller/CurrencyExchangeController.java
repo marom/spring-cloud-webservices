fa51319a01ed3604a1c5f41bdb4b0c319684d6c2
marom <maro.muszynski@gmail.com>
2020-01-04 18:41:21 +0000

    create a simple REST hardcoded retrieve currency exchange value request

---
+++ currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,17 @@
+package com.marom.currencyexchangeservice.controller;
+
+import com.marom.currencyexchangeservice.domain.ExchangeValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(4.98));
+    }
+}

8929255840d782618f2947bb332282c97da6c6fe
marom <maro.muszynski@gmail.com>
2020-01-05 15:22:54 +0000

    create another GET request and use Feign client proxy to call another service

--- currency-conversion-service/src/main/java/com/marom/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/marom/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.marom.currencyconversionservice.controller;
 
+import com.marom.currencyconversionservice.CurrencyExchangeServiceProxy;
 import com.marom.currencyconversionservice.domain.CurrencyConversion;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,12 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
 
+    private CurrencyExchangeServiceProxy feignProxy;
+
+    public CurrencyConversionController(CurrencyExchangeServiceProxy feignProxy) {
+        this.feignProxy = feignProxy;
+    }
+
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
@@ -23,6 +30,13 @@ public class CurrencyConversionController {
         ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
 
         CurrencyConversion response = responseEntity.getBody();
+        return new CurrencyConversion(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+    }
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion response = feignProxy.retrieveExchangeValue(from, to);
         return new CurrencyConversion(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 }

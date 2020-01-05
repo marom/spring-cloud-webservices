b3039e8fdbcc9d1049bece7d2870f5c848c81629
marom <maro.muszynski@gmail.com>
2020-01-05 12:27:03 +0000

    define an example data to insert to the database

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple, port) values (10001, 'GBP', 'PLN', 4.89, 0);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple, port) values (10002, 'EUR', 'GBP', 0.85, 0);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple, port) values (10003, 'GBP', 'BDT', 99.22, 0);
+insert into exchange_value (id, currency_from, currency_to, conversion_multiple, port) values (10004, 'GBP', 'RON', 5.19, 0);

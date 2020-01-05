c243758edfa3c345d45f9a9a8d8a64d27f9a986b
marom <maro.muszynski@gmail.com>
2020-01-05 12:53:41 +0000

    refactor get request to use repository to get data from a database

--- currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,25 +1,27 @@
 package com.marom.currencyexchangeservice.controller;
 
 import com.marom.currencyexchangeservice.domain.ExchangeValue;
+import com.marom.currencyexchangeservice.repository.ExchangeValueRepository;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
 
     private Environment environment;
+    private ExchangeValueRepository exchangeValueRepository;
 
-    public CurrencyExchangeController(Environment environment) {
+    public CurrencyExchangeController(Environment environment, ExchangeValueRepository exchangeValueRepository) {
         this.environment = environment;
+        this.exchangeValueRepository = exchangeValueRepository;
     }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(4.98));
+
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("server.port")));
         return exchangeValue;
     }

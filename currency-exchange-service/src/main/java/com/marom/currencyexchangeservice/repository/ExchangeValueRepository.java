e7e00bfa913655fb07f9fb819199a61e6d94126d
marom <maro.muszynski@gmail.com>
2020-01-05 12:46:07 +0000

    create a jpa repository for a Exchange Value bean

---
+++ currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/repository/ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.marom.currencyexchangeservice.repository;
+
+import com.marom.currencyexchangeservice.domain.ExchangeValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+}

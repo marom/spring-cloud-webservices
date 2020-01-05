09f9f43ba1998cadc0ad1db6520b3ef49c97823a
marom <maro.muszynski@gmail.com>
2020-01-05 14:51:13 +0000

    unhardcode request and make a request call using rest template to make a call to currency exchange service and then provide calculated total amount converted exchange

--- currency-conversion-service/src/main/java/com/marom/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/marom/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,18 +1,28 @@
 package com.marom.currencyconversionservice.controller;
 
 import com.marom.currencyconversionservice.domain.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(20001L, from, to, BigDecimal.ONE, quantity, BigDecimal.TEN, 0);
 
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion response = responseEntity.getBody();
+        return new CurrencyConversion(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 }

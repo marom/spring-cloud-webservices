579dfd0b1d3eb46758e9e67ca648f59104ddaa22
marom <maro.muszynski@gmail.com>
2020-01-05 15:08:21 +0000

    enable Feign to scan for a clients

--- currency-conversion-service/src/main/java/com/marom/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/marom/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,7 +2,9 @@ package com.marom.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.marom.currencyconversionservice")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 

82fe7438cc7a33b2f894075c5fa80d16879c3b99
marom <maro.muszynski@gmail.com>
2020-01-05 12:49:30 +0000

    define a sql query method to search exchange value table using from and to columns

--- currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/repository/ExchangeValueRepository.java
+++ currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/repository/ExchangeValueRepository.java
@@ -4,4 +4,6 @@ import com.marom.currencyexchangeservice.domain.ExchangeValue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+    ExchangeValue findByFromAndTo(String from, String to);
 }

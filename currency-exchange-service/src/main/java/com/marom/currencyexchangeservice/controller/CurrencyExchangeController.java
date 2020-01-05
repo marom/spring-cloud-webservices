96c8e46753c69088dd37d7fa60d1210c98126b80
marom <maro.muszynski@gmail.com>
2020-01-05 11:33:33 +0000

    use an environment property to get it from configuration properties file

--- currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/marom/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.marom.currencyexchangeservice.controller;
 
 import com.marom.currencyexchangeservice.domain.ExchangeValue;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,16 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(4.98));
-        exchangeValue.setPort(8000);
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("server.port")));
         return exchangeValue;
     }
 }

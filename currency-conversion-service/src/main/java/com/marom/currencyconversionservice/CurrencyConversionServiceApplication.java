821906cccbf01f4d2b41c4e2fbe3363ede2f3819
marom <maro.muszynski@gmail.com>
2020-01-11 16:44:06 +0000

    enable eureka client

--- currency-conversion-service/src/main/java/com/marom/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/marom/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.marom.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients("com.marom.currencyconversionservice")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
